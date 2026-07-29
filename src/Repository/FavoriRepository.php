27f6e2eb3863a274d82a4899b40361d9957d7cda
Roxane Lelievre <x-roxane.lelievre@hotmail.fr>
2026-07-29 13:13:22 +0200

    #22 #23 - Backend recherche/filtres recettes publiques, consultation, gestion des favoris

--- src/Repository/FavoriRepository.php
+++ src/Repository/FavoriRepository.php
@@ -3,6 +3,8 @@
 namespace App\Repository;
 
 use App\Entity\Favori;
+use App\Entity\Recette;
+use App\Entity\Utilisateur;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -14,5 +16,34 @@ class FavoriRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Favori::class);
+    }
+
+    /**
+     * Vérifie si l'utilisateur a déjà mis cette recette en favori 
+     */
+    public function findOneByUtilisateurEtRecette(Utilisateur $utilisateur, Recette $recette): ?Favori
+    {
+        return $this->createQueryBuilder('f')
+            ->andWhere('f.utilisateur = :utilisateur')
+            ->andWhere('f.recette = :recette')
+            ->setParameter('utilisateur', $utilisateur)
+            ->setParameter('recette', $recette)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
+
+    /**
+     * Retourne les recettes favorites de l'utilisateur (section "Mes favoris" du profil).
+     *
+     * @return Favori[]
+     */
+    public function findByUtilisateur(Utilisateur $utilisateur): array
+    {
+        return $this->createQueryBuilder('f')
+            ->andWhere('f.utilisateur = :utilisateur')
+            ->setParameter('utilisateur', $utilisateur)
+            ->orderBy('f.createdAt', 'DESC')
+            ->getQuery()
+            ->getResult();
     }
 }
